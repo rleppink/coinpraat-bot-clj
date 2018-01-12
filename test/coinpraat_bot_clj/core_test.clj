2a5328894f0fb959ca284e208ff25482a3e0347b
Ronald Leppink <ronaldleppink@gmail.com>
2018-01-12 19:09:11 +0100

    git init

---
+++ test/coinpraat_bot_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns coinpraat-bot-clj.core-test
+  (:require [clojure.test :refer :all]
+            [coinpraat-bot-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
