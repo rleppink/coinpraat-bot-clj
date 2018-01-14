2dab937a20569d0db08b42ff880aa2a03bcc5d79
Ronald Leppink <ronaldleppink@gmail.com>
2018-01-14 20:52:13 +0100

    Just testing stuff

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.4.474"]
+                 [http-kit "2.2.0"]
+                 [cheshire "5.8.0"]]
   :main ^:skip-aot coinpraat-bot-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/coinpraat_bot_clj/core.clj
+++ src/coinpraat_bot_clj/core.clj
@@ -1,7 +1,50 @@
 (ns coinpraat-bot-clj.core
+  (:require [clojure.core.async
+             :as async
+             :refer [>! <! go-loop chan]]
+            [org.httpkit.client :as http]
+            [cheshire.core :as json])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def bot-timeout 10)
+(defn bot-options [offset]
+  {:form-params {:allowed_updates "message"
+                 :offset offset
+                 :timeout bot-timeout}})
+
+(defn read-bot-token
+  "Reads the bot token from disk. This is a secret, so no hardcoding."
+  []
+  (clojure.string/trim (slurp "secret/bot-token")))
+
+(defn generate-bot-url
+  "Generate the bot's URL given the bot's token."
+  [bot-token]
+  (str "https://api.telegram.org/bot" bot-token "/"))
+
+(defn read-message
+  "Reads a message from Telegram bot."
+  [bot-url offset]
+  (let [options (bot-options offset)
+        updates-url (str bot-url "getUpdates")
+        response @(http/get updates-url options)]
+    response))
+
+(defn validate-response
+  "Validate a response from Telegram. Return response if valid, nil if invalid."
+  [response]
+  (when (and (= (:status response) 200)
+             (= (not-empty (:body response)))
+             (= (not-empty (:result (json/decode response true)))))))
+
+(defn find-update-id
+  "Find the chat update id in a Telegram response body."
+  [response]
+  (->> (:result response)
+       (first)
+       (:update_id)))
+
+(defn -main [& args]
+  (let [bot-token (read-bot-token)
+        bot-url (generate-bot-url bot-token)]
+    (read-message bot-url -1)))
